62abe56fdcd6708ef8e74d3dfb7d0ea90eb847a5
Vladimir <morozyan07@gmail.com>
2026-06-30 22:20:04 +0300

    fix: logo svg unoptimized to fix broken image

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -11,7 +11,7 @@ export default async function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link href="/" className="flex items-center">
-            <Image src="/logo.svg" alt="Ladoga Boat" width={140} height={56} priority />
+            <Image src="/logo.svg" alt="Ladoga Boat" width={140} height={56} priority unoptimized />
           </Link>
 
           <HeaderNav role={session?.role ?? null} />
