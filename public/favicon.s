ebdcce80dd4bc110594eb6b0a37f28f4f68cd478
morozyan07-ai <morozyan07@gmail.com>
2026-08-14 21:12:38 +0300

    Create favicon.s

---
+++ public/favicon.s
@@ -0,0 +1,7 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64">
+  <rect width="64" height="64" rx="10" fill="#0c1220"/>
+  <rect x="31" y="10" width="2" height="28" fill="#2dd4bf"/>
+  <polygon points="32,11 32,37 14,37" fill="#2dd4bf" opacity="0.9"/>
+  <polygon points="32,17 32,37 47,37" fill="#2dd4bf" opacity="0.6"/>
+  <path d="M11 40 L13 47 Q32 53 51 47 L53 40 Z" fill="#2dd4bf"/>
+</svg>
